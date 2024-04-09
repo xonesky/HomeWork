8196a6d8530bd13ba05022f8882b81c6f0a5968a
xonesky <148615863+xonesky@users.noreply.github.com>
2024-04-09 09:56:37 +0300

    antonholmes EP 2.2

---
+++ main.py
@@ -0,0 +1,23 @@
+﻿from datetime import date
+from datetime import datetime
+
+a = datetime(2020, 11, 4, 14, 53)
+print(a.strftime("%Y/%m/%d %H:%M:%S"))
+
+b = datetime(2020, 11, 4, 14, 53)
+print(b.strftime("%y/%B/%d %H:%M:%S %p"))
+
+c = datetime(2020, 11, 4, 14, 53)
+print(c.strftime("%a, %Y %b %d"))
+
+d = datetime(2020, 11, 4, 14, 53)
+print(d.strftime("%A, %Y %B %d"))
+
+e = datetime(2020, 11, 4, 14, 53)
+print(e.strftime("Weekday: %w"))
+
+f = datetime(2020, 11, 4, 14, 53)
+print(f.strftime("Day of the year: %j"))
+
+g = datetime(2020, 11, 4, 14, 53)
+print(g.strftime("Week number of the year: %U"))
